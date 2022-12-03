9df766dea5c0511f229ee39450a15919bb298ccc
Renan Cruz <renan.frcr@gmail.com>
2022-12-03 19:11:57 -0300

    Removendo o campo username de users

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,12 +5,11 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, username, email, password, driver_license } = request.body;
+    const { name, email, password, driver_license } = request.body;
     const createUserUserCase = container.resolve(CreateUserUseCase);
 
     await createUserUserCase.execute({
       name,
-      username,
       email,
       password,
       driver_license,
