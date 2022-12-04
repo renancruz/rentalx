0606a47232ebeb1f52444954ce53683454586aa4
Renan Cruz <renan.frcr@gmail.com>
2022-12-04 11:39:56 -0300

    tratamento de exceções

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repository/IUsersRepository";
 
 interface IRequest {
@@ -29,14 +30,14 @@ class AuthenticateUserUseCase {
     const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorrect!");
+      throw new AppError("Email or password incorrect!");
     }
 
     const passwordMath = await compare(password, user.password);
 
     // Senha está correta
     if (!passwordMath) {
-      throw new Error("Email or password incorrect!");
+      throw new AppError("Email or password incorrect!");
     }
 
     // Gerar jsonwebtoken
