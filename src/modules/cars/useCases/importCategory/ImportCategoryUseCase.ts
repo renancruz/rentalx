2c168a6ea4bfac85dae0db91dc980e8b788e6c01
Renan Cruz <renan.frcr@gmail.com>
2022-12-04 18:04:23 -0300

    Remover arquivo de avatar existente

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -20,6 +20,7 @@ class ImportCategoryUseCase {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportCategory[] = [];
+
       const parseFile = csvParse();
 
       stream.pipe(parseFile);
@@ -33,6 +34,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
