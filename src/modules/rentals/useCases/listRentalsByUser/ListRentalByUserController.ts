8d70da7523d8d45a9fead17a8c7b9a8d73d96141
Renan Cruz <renan.frcr@gmail.com>
2023-01-11 14:29:04 -0300

    Refatorando a listagem de aluguel do usuário

--- src/modules/rentals/useCases/listRentalsByUser/ListRentalByUserController.ts
+++ src/modules/rentals/useCases/listRentalsByUser/ListRentalByUserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { ListRentalsByUserUseCase } from "./ListRentalsByUser";
+import { ListRentalsByUserUseCase } from "./ListRentalsByUserUseCase";
 
 class ListRentalsByUserController {
   async handle(request: Request, response: Response): Promise<Response> {
